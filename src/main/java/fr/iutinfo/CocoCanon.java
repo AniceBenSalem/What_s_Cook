b4f3e2e6cd819aa44d661755004b00d6aeef98a8
Leakeur <mick.descamps@gmail.com>
2015-01-13 11:58:12 +0100

    cococanonv2

--- src/main/java/fr/iutinfo/CocoCanon.java
+++ src/main/java/fr/iutinfo/CocoCanon.java
@@ -3,7 +3,7 @@ package fr.iutinfo;
 public class CocoCanon extends BatimentDefensif{
 
 	public CocoCanon(Ile ile) {
-		super("CocoCanon", 10, 10, 400, 400, 400, 40); 
+		super("CocoCanon", 10, 10, 400, 400, 400, 40, ile); 
 		this.level = 0;
 		this.name = "CocoCanon";
 		ile.addBatiment(this);
