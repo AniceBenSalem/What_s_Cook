f2faed0b8825ed1693b7244093666c043e24e5e4
Leakeur <mick.descamps@gmail.com>
2015-01-13 11:55:25 +0100

    CocoCanon !!!!!!!!!!!!
    Je lance des noix de coco.

---
+++ src/main/java/fr/iutinfo/CocoCanon.java
@@ -0,0 +1,21 @@
+package fr.iutinfo;
+
+public class CocoCanon extends BatimentDefensif{
+
+	public CocoCanon(Ile ile) {
+		super("CocoCanon", 10, 10, 400, 400, 400, 40); 
+		this.level = 0;
+		this.name = "CocoCanon";
+		ile.addBatiment(this);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void amelioration() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+		
+}
