89c1a2aff663ed906db3e29294976c52695be1ae
Amaury Dumetz <amaury.dumetz@yahoo.fr>
2015-01-13 11:24:33 +0100

    test univers + repere

--- src/main/java/fr/iutinfo/Univers.java
+++ src/main/java/fr/iutinfo/Univers.java
@@ -5,21 +5,36 @@ import java.util.ArrayList;
 public class Univers {
 	private String nomUniver;
 	private ArrayList<Ile> listeIle;
-	private int [][] repere;
+	private Ile [][] repere;
 	
 	
 	public Univers (String nomUniver){
 		this.nomUniver = nomUniver;
 		listeIle = new ArrayList<Ile>();
-		this.repere = new int [200][200];
+		this.repere = new Ile [200][200];
+	}
+	
+	public void initialiseRepere () {
+		for (int i = 0; i < repere.length; i++) {
+			for (int j = 0; j < repere.length; j++) {
+				this.repere[i][j] = null;
+			}
+		}
 	}
 	
 	public ArrayList<Ile> getListeIle() {
 		return listeIle;
 	}
 	
-	public void addIle(Ile ile, int x, int y){
-		listeIle.add(ile);
+	public void addIle(Ile ile, int x, int y) throws PlacementOccupeException{
+		if (this.repere[x][y] == null) {
+			listeIle.add(ile);
+			repere[x][y] = ile;
+		}
+
+		else{
+			throw new PlacementOccupeException();
+		}
 		
 	}
 	
