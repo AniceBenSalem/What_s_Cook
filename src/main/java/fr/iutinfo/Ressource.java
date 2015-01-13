736111306d5e8dbb2d381012e16fb6cfcb436370
Henri Dubois <hendub62@laposte.net>
2015-01-13 10:42:55 +0100

    add image

--- src/main/java/fr/iutinfo/Ressource.java
+++ src/main/java/fr/iutinfo/Ressource.java
@@ -4,13 +4,4 @@ public abstract class Ressource {
 	
 	String nom;
 	int valeurBourse; // valeur en bourse, plus tard peut etre ?
-	
-	/**
-	 * Envoyer des ressources a l'ile ile
-	 * @param ile
-	 */
-
-	
-	
-
 }
