2c2837c571af4767793285d679521a77380962bb
Anice Ben Salem <anice.bensalem@hotmail.fr>
2015-01-13 10:28:34 +0100

    Test Univers

--- src/test/java/fr/iutinfo/UniversTest.java
+++ src/test/java/fr/iutinfo/UniversTest.java
@@ -2,34 +2,45 @@ package fr.iutinfo;
 
 import static org.junit.Assert.*;
 
+import javax.ws.rs.core.Application;
+
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
 public class UniversTest extends JerseyTest{
-
+	
+	
+	@Override
+    protected Application configure() {
+        return new App();
+        
+    }
+	
 	@Test
-	public void testGetMaxId() {
+	public void TestGetMaxId() {
 		Univers univ = new Univers("Omega");
 		Ile ile = new Ile(univ, "Ara..terroriste");
 		univ.addIle(ile);
 		univ.addIle(ile);
-		assertEquals(2,univ.getMaxId());
+		assertEquals(3,univ.getMaxId());
 	}
 	
-	public void testToString(){
-		Univers univ = new Univers("Omega");
-		Ile ile = new Ile(univ, "Ara..terroriste");
-		univ.addIle(ile);
-		univ.addIle(ile);
-		assertEquals( "l'univer Omega possede 2 ile(s).",univ.toString());
-	}
 	
-	public void testgetNomUnivers(){
+	
+	public void TestgetNomUnivers(){
 		Univers univ = new Univers("Omega");
 		Ile ile = new Ile(univ, "Ara..terroriste");
 		univ.addIle(ile);
 		univ.addIle(ile);
 		assertEquals( "Omega",univ.getNomUnivers());
+	}
+	
+	public void TestToString(){
+		Univers univ = new Univers("Omega");
+		Ile ile = new Ile(univ, "Ara..terroriste");
+		univ.addIle(ile);
+		univ.addIle(ile);
+		assertEquals( "l'univer Omega possede 2 ile(s).",univ.toString());
 	}
 	
 
