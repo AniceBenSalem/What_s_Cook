4ca33eb60ed84a1f0726399f442604fe6d25c528
Medhi BUSTIN <medhi-bustin@hotmail.fr>
2015-01-13 14:38:57 +0100

    site

---
+++ src/main/java/fr/iutinfo/BatimentTest.java
@@ -0,0 +1,14 @@
+package fr.iutinfo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BatimentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
