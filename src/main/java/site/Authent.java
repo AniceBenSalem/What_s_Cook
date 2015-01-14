a25a11ae38caaeb6c95b3e690106a7f88aa6743a
Medhi BUSTIN <medhi-bustin@hotmail.fr>
2015-01-14 16:58:13 +0100

    connection

--- src/main/java/site/Authent.java
+++ src/main/java/site/Authent.java
@@ -25,26 +25,26 @@ public class Authent extends HttpServlet{
 			session.setAttribute("identificateur",req.getParameter("login"));
 			session.setMaxInactiveInterval(10*60*60);
 			System.out.println(req.getParameter("login"));
-				
+
 			//Enregistrement du driver
 			Class.forName("org.sqlite.JDBC");			
 
 			//Connexion a la base
 			String url = "jdbc:sqlite:vinland.db";
-            String name = null;
-            String passwd = null;
-            try (Connection con = DriverManager.getConnection(url, name, passwd)) {
-            	System.out.println("ccccc");
-			
+			String name = null;
+			String passwd = null;
+			try (Connection con = DriverManager.getConnection(url, name, passwd)) {
+				System.out.println("ccccc");
+
 				//execution de la requete
 				Statement stmt = con.createStatement();
 				String query = "select * from users Where login = '" + req.getParameter("login") + "' AND password = '" + req.getParameter("password") + "'";
 				ResultSet rs = stmt.executeQuery(query);
-	
+
 				PrintWriter out = res.getWriter();
 				res.setContentType("text/html");
 				out.println("<head><link rel=\"icon\" type=\"image/png\" href=\"../IMG/favicon.png\" /><title>Authentification</title></head>"); //Iconne
-				
+
 				if(rs.next()){
 					session.setAttribute("role",rs.getString("role"));
 					if(session.getAttribute("url") != null)
@@ -53,17 +53,18 @@ public class Authent extends HttpServlet{
 						res.sendRedirect("Menu");    //A modifier
 				}else
 					System.out.println("coucou");
-					res.sendRedirect("index.html");
-				
+				session.setAttribute("idTrue",rs.getString("false"));
+				res.sendRedirect("../index.html");
+
 				//fermeture des espaces
 				con.close();
 			}catch(Exception e){
-                System.out.println("Problem requete");
-                System.out.println(e.getMessage());
-            }
+				System.out.println("Problem requete");
+				System.out.println(e.getMessage());
+			}
 		}catch(Exception e){
 			System.out.println("Probème : " + e.getMessage());
 		}
 	}
-	
+
 }
