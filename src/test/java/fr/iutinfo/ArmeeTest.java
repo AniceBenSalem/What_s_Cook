5132932acacae3d31b65d8be7bf46233b252f1c7
Leon VICTOR <leon.victor@etudiant.univ-lille1.fr>
2015-01-15 10:46:14 +0100

    Vol de ressources OK

--- src/test/java/fr/iutinfo/ArmeeTest.java
+++ src/test/java/fr/iutinfo/ArmeeTest.java
@@ -24,6 +24,8 @@ public class ArmeeTest extends JerseyTest{
 		u=new Univers("test");
 		i=new Ile(u,"test1",1,1);
 		i2=new Ile(u,"test2",2,2);
+		i.getGenerateurCoquillage().stopGenererRessource();
+		i2.getGenerateurCoquillage().stopGenererRessource();
 
 	}
 
@@ -33,7 +35,7 @@ public class ArmeeTest extends JerseyTest{
     }
 	
 	@Test
-	public void testVol(){
+	public void testVol() throws PlacementOccupeException{
 		i.setPoints(100);
 		i.getEntrepot().setCoquillage(100);
 		i2.setPoints(100);
@@ -44,21 +46,40 @@ public class ArmeeTest extends JerseyTest{
 	}
 	
 	@Test
-	public void testVolPetitRatio(){
+	public void testVolPetitRatio() throws PlacementOccupeException{
 		i.setPoints(100);
-		i.getEntrepot().setCoquillage(100);
 		i2.setPoints(15030);
+		
+		i.getEntrepot().setCoquillage(100);
 		i2.getEntrepot().setCoquillage(0);
+
 		i2.getArmee().volRessource(i);
+		
 		assertEquals(0,i2.getEntrepot().getCoquillage());
 		assertEquals(100,i.getEntrepot().getCoquillage());
 	}
 	
 	@Test
-	public void testVolGrosRatio(){
-		i.setPoints(15000);
-		i.getEntrepot().setCoquillage(100);
+	public void testVolMoyenRatio() throws PlacementOccupeException{
+		i.setPoints(110);
 		i2.setPoints(100);
+		
+		i.getEntrepot().setCoquillage(100);
+		i2.getEntrepot().setCoquillage(0);
+
+		i2.getArmee().volRessource(i);
+		
+		assertNotEquals(0,i2.getEntrepot().getCoquillage());
+		assertNotEquals(100,i.getEntrepot().getCoquillage());
+	}
+	
+	@Test
+	public void testVolGrosRatio() throws PlacementOccupeException{
+		i2.setPoints(100);
+		i.setPoints(15000);
+		
+		//i2 (peu de points) attaque i (beaucoup de points) = i2 vole beaucoup de ressources 
+		i.getEntrepot().setCoquillage(100);
 		i2.getEntrepot().setCoquillage(0);
 		i2.getArmee().volRessource(i);
 		assertEquals(40,i2.getEntrepot().getCoquillage());
@@ -74,6 +95,7 @@ public class ArmeeTest extends JerseyTest{
 	
 	@Test
 	public void testGetPV() {
+		
 		a=new Armee(i);
 		a.addUnite(new SurfeurCroMagnon());
 		a.addUnite(new SurfeurCroMagnon());
@@ -113,12 +135,10 @@ public class ArmeeTest extends JerseyTest{
 		i2 = new Ile(u,"un mechant",2,2);
 		i2.getEntrepot().setCoquillage(1000000);
 		i.upCococanon(); //Cococanon lvl 1 = 100 pv
-		System.out.println(i.getValeurDefense());
 		for(int i = 0;i<11;i++){
 			i2.upCromagnonSurfeur();
 		} // cree 11 cromagnon = 42pv, 10force (total = 462pv et 110 force)
 		i2.putAllSurfeurCromagnonArmee();
-		System.out.println(i2.getArmee().getForce());
 		boolean test =i2.getArmee().attaquerIle(i);
 		assertFalse(i2.getArmee().getStack().isEmpty()); // tous morts
 		assertTrue(test); //on attend une reussite de l'attaque
