b8a88a8d1ca82ae871010c06b304c840c07671f0
Leon VICTOR <leon.victor@etudiant.univ-lille1.fr>
2015-01-15 17:02:36 +0100

    ile jsp

---
+++ src/main/java/site/ServletIleJoueur.java
@@ -0,0 +1,60 @@
+package site;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet/ServletIleJoueur")
+
+public class ServletIleJoueur extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		try{
+			//Creation de la session
+			HttpSession session = req.getSession();
+
+			//Enregistrement du driver
+			Class.forName("org.sqlite.JDBC");			
+
+			//Connexion a la base
+			String url = "jdbc:sqlite:vinland.db";
+			String name = null;
+			String passwd = null;
+			try (Connection con = DriverManager.getConnection(url, name, passwd)) {
+				//execution de la requete
+				Statement stmt = con.createStatement();
+				String query = "select * from ile Where id = " + session.getAttribute("idIle");
+				ResultSet rs = stmt.executeQuery(query);
+				PrintWriter out = res.getWriter();
+				rs.next();
+				int idEntrepot = rs.getInt("idEntrepot");
+				req.setAttribute("proprietaire", rs.getString("proprietaire"));
+				query="select * from entrepot where id ="+idEntrepot;
+				rs=stmt.executeQuery(query);
+				rs.next();
+				req.setAttribute("entrepotcoquillage", rs.getInt("coquillage"));
+				req.setAttribute("entrepotcoquillagemax", rs.getInt("capacite"));
+					res.sendRedirect("MonIleJsp.jsp");
+					//session.setAttribute("idTrue",rs.getString("false"));
+				//fermeture des espaces
+				con.close();
+				this.getServletContext().getRequestDispatcher("MonIleJsp.jsp").forward(req,res);
+			}catch(Exception e){
+				System.out.println("Problem requete");
+				System.out.println(e.getMessage());
+			}
+		}catch(Exception e){
+			System.out.println("Probème : " + e.getMessage());
+		}
+	}
+
+}
---
+++ src/main/webapp/MonIleJsp.jsp
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" /> 
+		<LINK rel="stylesheet" href="style.css" type="text/css"> 
+		<LINK rel="stylesheet" href="css/bootstrap.css" type="text/css"> 
+		<link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico" />
+		<head><title>Gestion de l'ile</title></head>
+	</head>
+	<body>
+	<div class="container">
+	
+		<h1> <%req.getAttribute("proprietaire")%> </h1>
+		<table class="table table-bordered" style="background-color:#FFF">
+		<tr>
+			<th>Entrepots</th>
+			<th>Générateurs</th>
+			<th>Défenses</th>
+			<th>Caserne</th>
+		</tr>
+		<tr>
+			<td><img src="image/shell-icon.png" style="width:25px;height:25px"></img><%req.getAttribute("entrepotcoquillage") %>/
+			<%req.getAttribute("entrepotcoquillagemax") %><br>
+			</td>
+			<td><img src="image/shell-icon.png" style="width:25px;height:25px"></img>prod coquillage</td>
+			<td><img src="image/coconutcanon.png" style="width:25px;height:25px"></img> nb cococanons<br><br>
+				<img src="image/tiki.jpeg" style="width:25px;height:25px"></img> nb tiki
+			</td>
+			<td><img src="image/surfeur-icon.png" style="width:25px;height:25px"></img> nb cromagnons surfeurs<br><br>
+				<img src="image/icon-requin.png" style="width:25px;height:25px"></img> nb requins
+			</td>
+		</tr>
+		<tr>
+			<td><img src="image/icon-horloge.png" style="width:25px;height:25px"></img> temps upgrade</td>
+			<td><img src="image/icon-horloge.png" style="width:25px;height:25px"></img> temps upgrade</td>
+			<td><img src="image/icon-horloge.png" style="width:25px;height:25px"></img> temps upgrade</td>
+			<td><img src="image/icon-horloge.png" style="width:25px;height:25px"></img> temps upgrade</td>
+		</tr>
+		</table>
+		<table class="table table-bordered" style="background-color:#FFF">
+		</table>
+		</div>
+	</body>
+</html>
