7b5d2c60b65fe2aaa3be629d2c3db7538394ba00
hilmoinb <benjamin.hilmoine@etudiant.univ-lille1.fr>
2015-03-24 10:36:16 +0100

    recherche recette

--- whats/src/main/java/requetes/Base.java
+++ whats/src/main/java/requetes/Base.java
@@ -10,8 +10,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import org.apache.commons.net.ftp.FTPClient;
+// import org.apache.commons.net.ftp.FTPClient;
 import org.sqlite.SQLiteDataSource;
 
 public class Base {
