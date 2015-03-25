4e464663da683c1efc56f569f58fec1425569e06
ziyadib <bader.ziyadi@etudiant.univ-lille1.fr>
2015-03-25 14:08:11 +0100

    modifier evenement ok

--- whats/src/main/java/requetes/Evenement.java
+++ whats/src/main/java/requetes/Evenement.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,7 +20,7 @@ import fr.iutinfo.InitBDD;
 public class Evenement {
 	InitBDD b;
 	private static Requetes r ;
-
+	private static String nomModif="", lieuModif="";
 	@GET
 	@Path("/getEvenement/")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -52,18 +53,37 @@ public class Evenement {
 	 @GET
 		@Path("/modifEvent/{nom}/{lieu}")
 		@Produces(MediaType.APPLICATION_JSON)
-		public String modifEvent(@PathParam("nom")String nom,@PathParam("lieu") String ville) throws SQLException, IOException {
+		public String afficheMonEvent(@PathParam("nom")String nom,@PathParam("lieu") String ville) throws SQLException, IOException {
 		 Base b = new Base();
 			b.open();
+		
 			String retour =" { \"Event\" : [";
 			ResultSet rs = b.executeQry("Select * from Event where nom ='"+nom+"' AND ville ='"+ville+"';");
-		System.out.println("Select * from Event where nom ='"+nom+"' AND ville ='"+ville+"';");
+	
 			if(rs.next())
 			retour+= "{\"Nom\" : \""+rs.getString("nom")+"\" ,\"date\" : \""+rs.getString("date")+"\" , \"Lieu\" : \""+rs.getString("ville")+ "\" ,\"Desc\" : \""+rs.getString("description")+"\"}";
 		while(rs.next()){
 			retour+= ",{\"Nom\" : \""+rs.getString("nom")+"\" ,\"date\" : \""+rs.getString("date")+"\" , \"Lieu\" : \""+rs.getString("ville")+ "\" ,\"Desc\" : \""+rs.getString("description")+"\"}";
 		}
+		nomModif = nom;
+		 lieuModif = ville;
+		
 		return retour + "]}";
 		}
+	 
+
+	 @POST
+		@Path("/modifier/{newdesc}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public void modifEvent(@PathParam("newdesc") String newdesc) throws SQLException, IOException {
+		 Base b = new Base();
+			b.open();
+			String retour =" { \"Event\" : [";
+			System.out.println("Update Event set description='"+newdesc+"' where id=(select id from Event where nom ='"+nomModif+"' AND ville ='"+lieuModif+"');");
+		  b.executeStmt("Update Event set description='"+newdesc+"' where nom ='"+nomModif+"' AND ville ='"+lieuModif+"';");
+			
+		}
+
+	 
 
 }
