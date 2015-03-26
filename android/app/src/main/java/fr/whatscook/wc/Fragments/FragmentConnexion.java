c2cee6532c3ec896f1836ede17f1a1c4a253a955
hilmoinb <benjamin.hilmoine@etudiant.univ-lille1.fr>
2015-03-26 09:22:42 +0100

    session

--- android/app/src/main/java/fr/whatscook/wc/Fragments/FragmentConnexion.java
+++ android/app/src/main/java/fr/whatscook/wc/Fragments/FragmentConnexion.java
@@ -4,10 +4,12 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,12 +18,15 @@ import java.util.regex.Pattern;
 
 import fr.whatscook.wc.Acceuil;
 import fr.whatscook.wc.R;
+import fr.whatscook.wc.RequeteServeur;
 
 /**
  * Created by hilmoinb on 11/03/15.
  */
 public class FragmentConnexion extends Fragment {
-
+       public String name ="";
+       public String passwd="";
+       EditText login;
 
     public FragmentConnexion() {
 
@@ -32,7 +37,8 @@ public class FragmentConnexion extends Fragment {
                              Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_connexion, container, false);
-        final EditText login = (EditText) rootView.findViewById(R.id.email);
+
+       login = (EditText) rootView.findViewById(R.id.email);
         final EditText pass = (EditText) rootView.findViewById(R.id.passwd);
         final Button loginButton = (Button) rootView.findViewById(R.id.connect);
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +54,12 @@ public class FragmentConnexion extends Fragment {
                 } else if (passTxt.equals("")) {
                     pass.setError("error password");
                 } else {
-                    FragmentManager fm = getFragmentManager();
-                    if (fm != null) {
-                        FragmentTransaction ft = fm.beginTransaction();
-                        ft.replace(R.id.frame_container, new FragmentAcceuil());
-                        ft.commit();
-                    }
+
+                        name = loginTxt.toString();
+                        passwd = passTxt.toString();
+                        new JSONAndro().execute();
+
+
                 }
                 return;
             }
@@ -65,7 +71,6 @@ public class FragmentConnexion extends Fragment {
                 FragmentManager fm = getFragmentManager();
 
                 if (fm != null) {
-
                     FragmentTransaction ft = fm.beginTransaction();
                     ft.replace(R.id.frame_container, new FragmentCompte());
                     ft.commit();
@@ -79,6 +84,42 @@ public class FragmentConnexion extends Fragment {
 
         return rootView;
     }
+    private class JSONAndro extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+        @Override
+        protected String doInBackground(String... args) {
+            RequeteServeur t = new RequeteServeur();
+            // Getting JSON from URL
+            String s = t.getJSONFromUrl("http://bouleau22.iut-infobio.priv.univ-lille1.fr:8080/v1/connexion/android/"+name+"/"+passwd);
+            return s;
+        }
+        @Override
+        protected void onPostExecute(String json) {
+            if (json.equals("yes")){
+               Acceuil.editor.putString("login", name);
+               Acceuil.editor.putString("passwd", passwd);
+              FragmentManager fm = getFragmentManager();
+               if (fm != null) {
+                  FragmentTransaction ft = fm.beginTransaction();
+                  ft.replace(R.id.frame_container, new FragmentAcceuil());
+                     ft.commit();
+
+            } else {
+                   login.setError("mauvais mot de passe ou identifiant");
+               }
+        }
+    }
+}
+
+
+
+
+
 
 
 }
--- whats/src/main/java/requetes/ConnexionResource.java
+++ whats/src/main/java/requetes/ConnexionResource.java
@@ -17,15 +17,26 @@ import javax.ws.rs.core.MediaType;
 
 
 @Path("/connexion")
-@Produces(MediaType.APPLICATION_JSON)
+
 public class ConnexionResource {
 	
 	@GET
 	@Path("/{login}/{password}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public String connexion(@PathParam("login") String login, @PathParam("password") String password) throws SQLException, IOException {
 		Requetes r = new Requetes();
 		if(r.checkUser(login,password))
 			return "{\"boulet1\": true}";
 		return "{\"boulet1\": false}";
 	}
+
+@GET
+@Path("/android/{login}/{password}")
+@Produces(MediaType.TEXT_PLAIN)
+public String connexionA(@PathParam("login") String login, @PathParam("password") String password) throws SQLException, IOException {
+	Requetes r = new Requetes();
+	if(r.checkUser(login,password))
+		return "yes";
+	return "no";
+}
 }
