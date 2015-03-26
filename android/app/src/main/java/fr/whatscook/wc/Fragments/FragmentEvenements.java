cab88279d30a29be607864085d00bab973540627
Anice <anice.bensalem@hotmail.fr>
2015-03-26 03:31:51 +0100

    Pb max il fait chier

--- android/app/src/main/java/fr/whatscook/wc/Fragments/FragmentEvenements.java
+++ android/app/src/main/java/fr/whatscook/wc/Fragments/FragmentEvenements.java
@@ -2,6 +2,7 @@ package fr.whatscook.wc.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,31 @@ import fr.whatscook.wc.R;
 public class FragmentEvenements extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-       final View rootView = inflater.inflate(R.layout.fragment_evenements, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_evenements, container, false);
+        final TextView newEvent = (TextView)rootView.findViewById(R.id.newEvent);
+        TextView setEvent = (TextView)rootView.findViewById(R.id.setEvent);
+        TextView listEvent = (TextView)rootView.findViewById(R.id.listEvent);
+
+        newEvent.setText("jirfoif");
+        newEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println(newEvent.getText()+"prout");
+            }
+        });
+        newEvent.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                System.out.println(v.getText());
+
+                return true;
+
+            }
+        });
+
   /*      ScrollView scroll = (ScrollView) rootView.findViewById(R.id.scrollEvent);
 
         TextView[] tv = new TextView[4];
--- whats/src/main/webapp/js/recettes.js
+++ whats/src/main/webapp/js/recettes.js
@@ -1,4 +1,6 @@
 var texte = "";
+var titre = "";
+var recette = "";
 $(document).ready(
 		$('#BoutonDeRecherche').click(function(event){
 		
@@ -12,12 +14,18 @@ $(document).ready(
 			$('.panel-body > h2').remove();
 			$('.panel-body > h3').remove();
 			$('.panel-body > hr').remove();
+			$('.panel-body > button').remove();
 			
 			for (var i= 0; i< json.Recettes.length; i++){
 				$('.panel-body').append('<h2>'+json.Recettes[i].TitreRecette+'</h2>'+'<h3>'+json.Recettes[i].TxtRecette+'</h3>');
-				if(readCookie("login") !== null)
-					$('.panel-body').append("<center><button id=\"partager"+i+"\" class=\"btn btn-primary\" onclick=\"partagerRecette('"+json.Recettes[i].TitreRecette+"')\">Partager</button>          <button id=\"favoris"+i+"\" class=\"btn btn-danger\" onclick=\"ajouterFavoris('"+json.Recettes[i].TitreRecette+"','"+json.Recettes[i].TxtRecette+"')\">Favoris</button></center>");
-               	$('.panel-body').append('<hr>');	
+				if(readCookie("login") !== null){
+					var prout = "ajouterFavoris(\""+json.Recettes[i].TxtRecette+"\")";
+					console.log(prout);
+					//$('.panel-body').append("<center><button id=\"partager"+i+"\" class=\"btn btn-primary\"  onclick=\"partagerRecette('"+titre.toString()+"')\">Partager</button>  <button id=\"favoris"+i+"\" class=\"btn btn-danger\" onclick=\"ajouterFavoris('"+titre.toString()+"','"+recette.toString()+"')\">Favoris</button></center>");
+					$('.panel-body').append("<center><button id='partager"+i+"' class='btn btn-primary'  onclick='partagerRecette(\""+titre+"\")'>Partager</button>  <button id='favoris"+i+"' class='btn btn-danger' onclick ='ajouterFavoris("+json+")'	 >Favoris</button></center>");
+					
+					$('.panel-body').append('<hr>');
+				}
 				
 			} 
 		 },
@@ -29,15 +37,22 @@ $(document).ready(
 );
 
 
-function ajouterFavoris(titre,recette) {
+function ajouterFavoris(json){
+	var titre = json.Recettes[i].TitreRecette;
+	var recette = json.Recettes[i].TxtRecette;
+	console.log(titre);
+	console.log(recette);
 	var login = readCookie("login");
+	if(titre != "" && recette != ""){
 	$.ajax({
 	    url:"http://localhost:8080/v1/cook/ajouterFavoris/"+login+"/"+titre+"/"+recette,
 		type: "GET",
 		datatype:'APPLICATION_JSON',
 		success: function(json) {
-		},
+			console.log("coucou");
+		}
 	});
+	}
 }
 
 
@@ -49,6 +64,7 @@ function partagerRecette(titre) {
 		type: "GET",
 		datatype:'APPLICATION_JSON',
 		success: function(json) {
-		},
+			console.log("coucou");
+		}
 	});
 }
